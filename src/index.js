7747d79873fc9c78b6a3b251d51762799b1d028a
Vivan <vivanpanda13@gmail.com>
2023-07-27 20:45:15 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './assets/styles/style.css'
+// import ExampleImage from './assets/img/exampleimage.png';
+
+// const Image = new Image();
+// Image.src = ExampleImage;
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',  
+  entry: {
+    bundle: path.resolve(__dirname, 'src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    open: {
+      app: {
+        name: 'Google Chrome'
+      }
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(csv|tsv)$/i,
+        use: ['csv-loader'],
+      },
+      {
+        test: /\.xml$/i,
+        use: ['xml-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Document',
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
