c35e09250cb74ed20f3c5804e70799800d62601c
Vivan <vivanpanda13@gmail.com>
2023-07-28 16:35:11 +0400

    Practice with pushing and creating new projects & todos

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import { Project } from './project'
 import { Todo } from './todo'
+
+let projects = [];
+
+// Test Project with Todos
+
+// let testProject = new Project('testProject')
+// projects.push(testProject)
+// testProject.tasks.push(new Todo('testTodo', 'This is a test Todo.', 'Tomorrow', 'Medium', 'testProject'))
+// console.log(projects);
+
