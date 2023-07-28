e0ced4dd1e5ff2044aba7a827861caef120b018b
Vivan <vivanpanda13@gmail.com>
2023-07-28 13:56:45 +0400

    Create todo class and constructor with properties

---
+++ src/todo.js
@@ -0,0 +1,29 @@
+export class Todo{
+    constructor(title, description, dueDate, priority, project) {
+        this.title = title
+        this.description = description
+        this.dueDate = dueDate
+        this.priority = priority
+        this.project = project
+    }
+
+    getTitle() {
+        return this.title;
+    }
+
+    getDescription() {
+        return this.description;
+    }
+
+    getDueDate() {
+        return this.dueDate;
+    }
+
+    getPriority() {
+        return this.priority;
+    }
+
+    getProject() {
+        return this.project;
+    }
+}
