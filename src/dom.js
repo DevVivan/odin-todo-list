fd8a492b0ec82b91e40cc683bb64ddfeadb84a5b
Vivan <vivanpanda13@gmail.com>
2023-07-30 11:20:46 +0400

    Setup modals

--- src/dom.js
+++ src/dom.js
@@ -2,11 +2,23 @@ import { Project } from './project'
 import { Todo } from './todo'
 import './assets/styles/style.css'
 
-const newProjectButton = document.querySelector('.new-project-button');
-const newProjectModal = document.querySelector('.new-project-modal');
+export function createModalForms() {
+    // Modal for new projects
 
-export function showProjectModal() {
+    const newProjectButton = document.querySelector('.new-project-button');
+    const newProjectModal = document.querySelector('.new-project-modal');
+
     newProjectButton.addEventListener('click', () => {
         newProjectModal.showModal()
     })
+
+    // Modal for new todos
+
+    const newTodoButton = document.querySelector('.new-todo-container');
+    const newTodoModal = document.querySelector('.new-todo-modal');
+
+    newTodoButton.addEventListener('click', () => {
+        newTodoModal.showModal()
+    })
+
 }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import { Project } from './project'
 import { Todo } from './todo'
-import { showProjectModal } from './dom';
+import { createModalForms } from './dom';
 import './assets/styles/style.css'
 
 let projects = [];
 
-// Test Project with Todos
-
-// let testProject = new Project('testProject')
-// projects.push(testProject)
-// testProject.tasks.push(new Todo('testTodo', 'This is a test Todo.', 'Tomorrow', 'Medium', 'testProject'))
-// console.log(projects);
-
-
-showProjectModal()
+createModalForms()
