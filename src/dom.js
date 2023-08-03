0511fd2aa0a139dbca9fe564b8933c343577e880
Vivan <vivanpanda13@gmail.com>
2023-08-03 18:52:38 +0400

    Adjust modal code between modules

--- src/dom.js
+++ src/dom.js
@@ -1,33 +1,3 @@
 import { Project } from './project'
 import { Todo } from './todo'
 import './assets/styles/style.css'
-
-export function createModalForms() {
-    // Modal for new projects
-
-    const newProjectButton = document.querySelector('.new-project-button');
-    const newProjectModal = document.querySelector('.new-project-modal');
-    const closeProjectModalButton = document.querySelector('.close-project-modal')
-
-    newProjectButton.addEventListener('click', () => {
-        newProjectModal.showModal();
-    })
-
-    closeProjectModalButton.addEventListener('click', () => {
-        newProjectModal.close();
-    })
-
-    // Modal for new todos
-
-    const newTodoButton = document.querySelector('.new-todo-container');
-    const newTodoModal = document.querySelector('.new-todo-modal');
-    const closeTodoModalButton = document.querySelector('.close-todo-modal')
-
-    newTodoButton.addEventListener('click', () => {
-        newTodoModal.showModal()
-    })
-
-    closeTodoModalButton.addEventListener('click', () => {
-        newTodoModal.close();
-    })
-}
