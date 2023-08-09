b3ddc54a83a3a1ccdf5716236ff83feed78764a4
Vivan <vivanpanda13@gmail.com>
2023-08-09 12:23:30 +0400

    Organize projects logic into function

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import './assets/styles/style.css'
 import { createContentUsingDOM } from './dom';
 
 let projects = [new Project('Web Development')];
+const newProjectForm = document.querySelector('.new-project-form');
+const projectTabs = document.querySelector('.project-tabs');
+const newProjectModal = document.querySelector('.new-project-modal');
 
 export function createModalForms() {
     // Modal for new projects
 
     const newProjectButton = document.querySelector('.new-project-button');
-    const newProjectModal = document.querySelector('.new-project-modal');
-    const newProjectForm = document.querySelector('.new-project-form');
     const closeProjectModalButton = document.querySelector('.close-project-modal');
-    const projectTabs = document.querySelector('.project-tabs');
 
     newProjectButton.addEventListener('click', () => {
         newProjectModal.showModal();
@@ -20,16 +20,6 @@ export function createModalForms() {
 
     closeProjectModalButton.addEventListener('click', () => {
         newProjectModal.close();
-    })
-
-    newProjectForm.addEventListener('submit', (event) => {
-        newProjectModal.close();
-        event.preventDefault();
-        let projectName = document.getElementById("project-name");
-        projectName = projectName.value;
-        projects.push(new Project(projectName))
-        projectTabs.innerHTML = '';
-        createContentUsingDOM(projects);
     })
 
     // Modal for new todos
@@ -52,5 +42,18 @@ export function createModalForms() {
     })
 }
 
+export function createProjects() {
+    newProjectForm.addEventListener('submit', (event) => {
+        newProjectModal.close();
+        event.preventDefault();
+        let projectName = document.getElementById("project-name");
+        projectName = projectName.value;
+        projects.push(new Project(projectName))
+        projectTabs.innerHTML = '';
+        createContentUsingDOM(projects);
+    })
+}
+
 createModalForms()
+createProjects()
 createContentUsingDOM(projects);
