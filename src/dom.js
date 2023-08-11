6aba9f2bd2a2a976a8353bcd239900fbaf7dc1d3
Vivan <vivanpanda13@gmail.com>
2023-08-11 13:10:47 +0400

    Begin with project switching logic

--- src/dom.js
+++ src/dom.js
@@ -7,16 +7,44 @@ export function createContentUsingDOM(projects) {
     
     projects.forEach(project => {
         const projectTab = document.createElement('div');
-        projectTab.className = 'project-tab';
+        projectTab.classList.add = 'project-tab';
         
         const projectTitle = document.createElement('h3');
-        projectTitle.className = 'project-sidebar-title';
+        projectTitle.classList.add = 'project-sidebar-title';
         projectTitle.textContent = project.getName();
         
         projectTab.appendChild(projectTitle);
         projectTabs.appendChild(projectTab);
 
         projectTab.addEventListener('click', () => {
+            let mainContainer = document.querySelector('.main-container');
+            mainContainer.innerHTML = ''
+
+            let contentHeaderContainer = document.createElement('div');
+            contentHeaderContainer.classList.add('content-header-container')
+            mainContainer.appendChild(contentHeaderContainer);
+
+            let contentTitle = document.createElement('h1');
+            contentTitle.classList.add('content-title')
+            contentTitle.textContent = project.getName();
+            contentHeaderContainer.appendChild(contentTitle);
+
+            let newTodoContainer = document.createElement('div');
+            newTodoContainer.classList.add('new-todo-container')
+            contentHeaderContainer.appendChild(newTodoContainer);
+
+            let newTodoIcon = document.createElement('i')
+            newTodoIcon.classList.add('fa-solid')
+            newTodoIcon.classList.add('fa-plus')
+            newTodoIcon.classList.add('fa-lg')
+            newTodoIcon.classList.add('fa-2x')
+            newTodoIcon.classList.add('new-todo-button')
+            newTodoContainer.appendChild(newTodoIcon);
+
+            let contentHr = document.createElement('hr');
+            contentHr.classList.add('content-hr')
+            mainContainer.appendChild(contentHr);
+
             console.log('A project has been clicked.')
         })
     });
